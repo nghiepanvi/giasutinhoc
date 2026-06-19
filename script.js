96d603588e258eeaf2a369716d014d7e84ace454
nghiepanvi <nghiepanvi@gmail.com>
2026-06-19 11:21:42 +0700

    doi ten file

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 
-   document
-            .getElementById("registerForm")
-            .addEventListener("submit", function (e) {
-                e.preventDefault();
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", async function (e) {
 
-                let name = document.getElementById("studentName").value;
+    e.preventDefault();
 
-                let lop = document.getElementById("className").value;
+    const data = {
+      studentName: document.getElementById("studentName").value,
+      className: document.getElementById("className").value,
+      phone: document.getElementById("phone").value,
+      subject: document.getElementById("subject").value,
+      note: document.getElementById("note").value
+    };
 
-                let phone = document.getElementById("phone").value;
+    try {
 
-                let subject = document.getElementById("subject").value;
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbxaYv-NxboV-sTY3xSJJ-phkQ9q0H3FsFE_75VjsMto7rWghhig_q2Ujja8tTLlN-v3ew/exec",
+        {
+          method: "POST",
+          body: JSON.stringify(data)
+        }
+      );
 
-                alert(
-                    "Đăng ký thành công!\n\n" +
-                    "Học sinh: " +
-                    name +
-                    "\n" +
-                    "Lớp: " +
-                    lop +
-                    "\n" +
-                    "SĐT PH: " +
-                    phone +
-                    "\n" +
-                    "Môn học: " +
-                    subject,
-                );
-            });
+      alert("Đăng ký thành công!");
+
+      document.getElementById("registerForm").reset();
+
+    } catch (error) {
+
+      alert("Có lỗi xảy ra khi gửi dữ liệu!");
+
+      console.error(error);
+    }
+
+  });
